2eb2d861e4ee10a310a2621c2279528919dc8b40
Tiago Vidal <tiagod3p@gmail.com>
2021-04-21 17:29:13 -0300

    feat: add function to do trades with bitpreco api

--- app.py
+++ app.py
@@ -9,3 +9,14 @@ def info_coin(cryptoName):
     response = dict(requests.get(
         f'{DOMAIN}/{cryptoName.lower()}-brl/ticker').json())
     return response
+
+
+def trade(data):
+    headers = {'Content-Type': 'application/json'}
+    response = requests.post(
+        f'{DOMAIN}/trading/', headers=headers, data=data).json()
+    if response['success'] is True:
+        return response
+    else:
+        raise Exception('api-call err', response['message_cod'], asctime(localtime()))
+
