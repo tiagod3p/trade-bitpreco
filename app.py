a8e2934178c3d00eb90bf8aa462cf25678911417
Tiago Vidal <tiagod3p@gmail.com>
2021-04-21 17:31:23 -0300

    feat: add function to buy and sell cryptos

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ API_KEY = ""
 SIGNATURE = ""
 AUTH_TOKEN = API_KEY + SIGNATURE
 
-def info_coin(cryptoName):
+def info_crypto(cryptoName):
     response = dict(requests.get(
         f'{DOMAIN}/{cryptoName.lower()}-brl/ticker').json())
     return response
@@ -20,3 +20,14 @@ def trade(data):
     else:
         raise Exception('api-call err', response['message_cod'], asctime(localtime()))
 
+
+def buy(crypto, buy_price, buy_amount_crypto):
+    data_buy = json.dumps({"cmd": "buy", "market": f"{crypto.upper()}-BRL", "price": buy_price,
+                           "amount": buy_amount_crypto, "auth_token": AUTH_TOKEN})
+    return api_call(data_buy)['order_id']
+
+
+def sell(crypto, sell_price, sell_amount_crypto):
+    data_sell = json.dumps({"cmd": "sell", "market": f"{crypto.upper()}-BRL", "price": sell_price,
+                            "amount": sell_amount_crypto, "auth_token": AUTH_TOKEN})
+    return api_call(data_sell)
