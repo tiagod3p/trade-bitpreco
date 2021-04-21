7fb51a879ded5f5eb159902bc4109f304293a0df
Tiago Vidal <tiagod3p@gmail.com>
2021-04-21 17:24:11 -0300

    feat: add function to get informations about a crypto

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import requests
 
-DOMAIN = 'api-simulator.bitpreco.com' ## dev environment bitpreco
+DOMAIN = 'https://api-simulator.bitpreco.com' ## dev environment bitpreco
 API_KEY = ""
 SIGNATURE = ""
 AUTH_TOKEN = API_KEY + SIGNATURE
 
+def info_coin(cryptoName):
+    response = dict(requests.get(
+        f'{DOMAIN}/{cryptoName.lower()}-brl/ticker').json())
+    return response
